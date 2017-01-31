8a4b009dd23bff43ab265bf151c146090dd6ec55
jimmytan <jimmytan119@hotmail.com>
2017-01-30 22:38:47 -0500

    start to code menu

---
+++ app/menu/baSidebar.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by wenjuntan on 2017-01-30.
+ */
+import {Component, OnInit} from "@angular/core";
+@Component({
+    selector: 'ba-sidebar',
+    template: require('./baSidebar.html')
+})
+export class BaSidebar implements OnInit{
+
+
+    public ngOnInit(): void {
+    }
+
+}
