6a7f096b73f3b862fca4a660710850e0f0ded13f
jimmytan <jimmytan119@hotmail.com>
2017-02-11 18:04:00 -0500

    menu

---
+++ app/menu/baMenu.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by wenjuntan on 2017-02-06.
+ */
+import {Component} from "@angular/core";
+@Component({
+    selector: "ba-menu",
+    styles: [require('./baMenu.scss')];
+    temp
+})
